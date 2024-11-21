6d7b89b0907fb51600875811bf4df2a7ddeb3daa
Abelas-Solihin <abelassolihin@gmail.com>
2024-11-21 15:48:34 +0700

    modifikasi

--- hitungTotalHarga02.java
+++ hitungTotalHarga02.java
@@ -5,10 +5,10 @@ public class hitungTotalHarga02 {
         
         int hargaTotal = hargaItems[pilihMenu- 1] * banyakItem;
         if (kodePromo.equals("DISKON50")) {
-            hargaTotal = (int)(hargaTotal * 0.5);  // Diskon 50%
+            hargaTotal = (int)(hargaTotal * 0.5);  
             System.out.println("Berikan diskon 50%");
         } else if (kodePromo.equals("DISKON30")) {
-            hargaTotal = (int)(hargaTotal * 0.7);  // Diskon 30%
+            hargaTotal = (int)(hargaTotal * 0.7);  
             System.out.println("Berikan diskon 30%");
         } else {
             System.out.println("Kode promo tidak valid");
@@ -18,13 +18,18 @@ public class hitungTotalHarga02 {
     
     public static void main (String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("\nMasukkan menu yang ingin Anda pesan");
+        System.out.println("1. Kopi Hitam - Rp 15,000");
+        System.out.println("2. Cappuccino - Rp 20,000");
+        System.out.println("3. Latte - Rp 12,000");
+        System.out.println("4. Teh Tarik - Rp 10,000");
+        System.out.println("5. Mie Goreng - Rp 18,000");
+        System.out.println("\nMasukkan menu yang ingin Anda pesan : ");
         int pilihanMenu = sc.nextInt();
 
         System.out.print("Masukkan jumlah item yang ingin dipesan: ");
         int banyakItem = sc.nextInt();
 
-        System.out.print("Masukkan kode promo (jika ada): ");
+        System.out.print("Masukkan kode promo : ");
         String kodePromo = sc.next();
 
         int totalHarga = hitungTotalHarga(pilihanMenu, banyakItem, kodePromo);
